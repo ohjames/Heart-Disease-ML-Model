9ed3859afcf34570c1266763e99fc90b76d4180d
ohjames <nguyenjames0404@gmail.com>
2021-07-19 13:33:48 -0700

    tensorflow test

---
+++ test.py
@@ -0,0 +1,9 @@
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+import tensorflow as tf
+print(tf.test.is_gpu_available())
+print(tf.test.is_built_with_cuda())
